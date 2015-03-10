163d0dc3ff9e0d7e30644bfb6cb252a3fa8b5498
Piotr Kaminski <piotr@ideanest.com>
2015-03-09 21:05:16 -0700

    Moved code out of Reviewable, added README.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# digest-hud
-Digest performance analysis HUD for AngularJS.
+# Angular Digest HUD
+
+Digest performance analysis HUD for AngularJS.  To turn it on for your app:
+
+```
+angular.module('myApp', ['digestHud']).config(function(digestHudProvider) {
+  digestHudProvider.enable();
+  // Optional configuration settings:
+  digestHudProvider.numTopWatches = 20;  // number of items to display in detailed table
+  digestHudProvider.numDigestStats = 25;  // number of most recent digests to use for min/med/max stats
+});
+```
+
+The Digest HUD will have a performance impact on your code and there's no way to disable it once enabled, so as a rule you probably don't want to turn it on in production.  When it's enabled you'll see a small digest summary fixed in the lower-right corner of the window:
+
+![collapsed Digest HUD](https://raw.githubusercontent.com/pkaminski/digest-hud/master/readme_images/collapsed.png)
+
+The numbers show the minimum, median, and maximum running time of the last 25 (or `numDigestStats`) digests.  At the end of the line a dot blinks on or off every time a digest runs.  Together this gives you a quick idea of how hard your app's digest cycle is working.
+
+You can click on the summary to show a snapshot of more detailed stats:
+
+![collapsed Digest HUD](https://raw.githubusercontent.com/pkaminski/digest-hud/master/readme_images/expanded.png)
+
+This shows the top 20 (or `numTopWatches`) items that are slowing down your digest.  Each item leads with the percentage of total digest processing time that was spent on it, which is then broken down into three parts:  "watch" time spent evaluating the watch expression, "work" time spent running the listener, and "overhead" time spent in Angular's core code not captured by the other two (e.g., comparing or copying values).  Not everything that executes during a digest is a watcher, though.  You also get `$evalAsync`  and `$applyAsync` functions, and `$q` promise callbacks, where some of the timing parts are not applicable and will always be zero.  There's also the special placeholder `$ng-overhead` which stands in for time spent in the digest loop that we can't attribute to any of your code&mdash;for example, time spent traversing the scope hierarchy.
+
+You can hover over any line to get a popup with the full, properly-formatted function if it doesn't fit in the table.  You can also copy the table onto the clipboard by hitting command-c (or ctrl-c) when the table has focus, for example right after expanding it.
+
+At the bottom of the table there's a line that summarizes total digest processing time sampled so far, and the percentage of it represented in the table.  In the top-right corner, there are two controls that allow you to refresh the table from the latest data (this happens automatically whenever you expand the widget) or to reset the data gathered so far and start sampling from scratch.
+
+One thing to note is that in some rare cases one item may be part of another's execution.  When this happens, the running time of the nested item is not counted in its parent.
---
+++ digest-hud.js
@@ -0,0 +1,344 @@
+angular.module('digestHud', [])
+
+.provider('digestHud', function($provide) {
+  'use strict';
+
+  function WatchTiming(key) {
+    this.key = key;
+    this.reset();
+  }
+
+  WatchTiming.prototype.reset = function() {
+    this.watch = 0;
+    this.handle = 0;
+    this.overhead = 0;
+    this.total = 0;
+    this.cycleTotal = 0;
+    this.cycleStart = null;
+    this.subTotal = 0;
+  };
+
+  WatchTiming.prototype.startCycle = function(start) {
+    timingStack.push(this);
+    this.cycleStart = start;
+    this.cycleTotal = 0;
+    this.subTotal = 0;
+  };
+
+  WatchTiming.prototype.countTime = function(counter, duration) {
+    this[counter] += duration - this.subTotal;
+    this.cycleTotal += duration;
+    this.subTotal = 0;
+  };
+
+  WatchTiming.prototype.endCycle = function() {
+    if (!this.cycleStart) return;
+    var duration = Date.now() - this.cycleStart;
+    this.overhead += duration - this.cycleTotal;
+    this.cycleStart = null;
+    timingStack.pop();
+    if (timingStack.length) {
+      _.last(timingStack).subTotal += duration;
+    } else {
+      overheadTiming.overhead -= duration;
+    }
+  };
+
+  WatchTiming.prototype.sum = function() {
+    this.total = this.watch + this.handle + this.overhead;
+  };
+
+  WatchTiming.prototype.format = function(grandTotal) {
+    return percentage(this.total / grandTotal) + '\u2003(' +
+      percentage(this.watch / grandTotal) + ' + ' +
+      percentage(this.handle / grandTotal) + ' + ' +
+      percentage(this.overhead / grandTotal) +
+      ')\u2003' + this.key;
+  };
+
+  function flushTimingCycle() {
+    if (timingStack.length) _.last(timingStack).endCycle();
+  }
+
+  var digestTimings = [];
+  var watchTimings = {};
+  var keyOverride, timingStack;
+  var overheadTiming = createTiming('$$ng-overhead');
+  var digestHud = this;
+  var inDigest = false;
+
+  this.numTopWatches = 20;
+  this.numDigestStats = 25;
+
+  this.enable = function() {
+    var toggle = false;
+    var detailsText = '';
+
+    var element = $('<div></div>');
+    var buttonsElement = $(
+      '<div>' +
+      '<span id="digestHud-refresh">refresh</span> &bull; ' +
+      '<span id="digestHud-reset">reset</span> ' +
+      '</div>').appendTo(element);
+    var summaryElement = $('<div></div>').appendTo(element);
+    var detailsElement = $('<div></div>').appendTo(element);
+    var showDetails = false;
+    element.on('click', function() {
+      showDetails = !showDetails;
+      buttonsElement.toggle(showDetails);
+      detailsElement.toggle(showDetails);
+      if (showDetails) refreshDetails();
+    });
+    element.on('copy', function(ev) {
+      ev.originalEvent.clipboardData.setData('text/plain', detailsText);
+      ev.preventDefault();
+    });
+    buttonsElement.find('#digestHud-refresh').on('click', refreshDetails);
+    buttonsElement.find('#digestHud-reset').on('click', resetTimings);
+    buttonsElement.on('click', function(ev) {ev.stopPropagation();});
+    element.on('mousedown mouseup click', function(ev) {ev.stopPropagation();});
+    element.css({
+      position: 'fixed',
+      bottom: 0,
+      right: 0,
+      backgroundColor: 'rgba(0, 0, 0, 0.65)',
+      color: 'white',
+      padding: '2px 5px',
+      fontSize: 'small',
+      cursor: 'default',
+      zIndex: '1000000'
+    });
+    buttonsElement.css({
+      float: 'right',
+      display: 'none'
+    });
+    buttonsElement.children().css({
+      cursor: 'pointer'
+    });
+    detailsElement.css({
+      whiteSpace: 'pre',
+      minWidth: '30em',
+      maxWidth: '50em',
+      display: 'none'
+    });
+    $('body').append(element);
+
+    function refreshDetails() {
+      var grandTotal = 0, topTotal = 0;
+      var topWatchTimings = _.chain(watchTimings).values()
+        .each(function(timing) {timing.sum(); grandTotal += timing.total;})
+        .sortBy('total').reverse().first(digestHud.numTopWatches).value();
+      var lines = _.map(topWatchTimings, function(timing) {
+        topTotal += timing.total;
+        return timing.format(grandTotal);
+      });
+      var rows = _.map(lines, function(text) {
+        var row = $('<div></div>');
+        row.css({
+          overflow: 'hidden',
+          textOverflow: 'ellipsis'
+        });
+        row.text(text.replace(/[ \n]+/g, ' '));
+        row.attr('title', text.slice(29));
+        return row;
+      });
+      detailsElement.empty();
+      detailsElement.append(rows);
+      $('<div></div>').text(
+        'Top ' + topWatchTimings.length + ' items account for ' +
+        percentage(topTotal / grandTotal) + ' of ' + grandTotal + 'ms of digest processing time.'
+      ).appendTo(detailsElement);
+      detailsText = _.map(lines, function(text) {
+        return text.replace(/[ \n]+/g, ' ');
+      }).join('\n') + '\n';
+    }
+
+    function resetTimings() {
+      digestTimings = [];
+      _.invoke(watchTimings, 'reset');
+    }
+
+    $provide.decorator('$rootScope', ['$delegate', function($delegate) {
+      var proto = Object.getPrototypeOf($delegate);
+      var originalDigest = proto.$digest;
+      var originalEvalAsync = proto.$evalAsync;
+      var originalApplyAsync = proto.$applyAsync;
+      var originalWatch = proto.$watch;
+      var originalWatchGroup = proto.$watchGroup;
+      // $watchCollection delegates to $watch, no extra processing necessary
+      proto.$digest = instrumentedDigest;
+      proto.$evalAsync = instrumentedEvalAsync;
+      proto.$applyAsync = instrumentedApplyAsync;
+      proto.$watch = instrumentedWatch;
+      proto.$watchGroup = instrumentedWatchGroup;
+
+      function instrumentedDigest() {
+        // jshint validthis:true
+        this.$$postDigest(flushTimingCycle);
+        timingStack = [];
+        var start = Date.now();
+        inDigest = true;
+        try {
+          originalDigest.call(this);
+        } finally {
+          inDigest = false;
+        }
+        var duration = Date.now() - start;
+        overheadTiming.overhead += duration;
+        toggle = !toggle;
+        digestTimings.push(duration);
+        if (digestTimings.length > digestHud.numDigestStats) digestTimings.shift();
+        var len = digestTimings.length;
+        var sorted = _.sortBy(digestTimings);
+        var median = len % 2 ?
+          sorted[(len - 1) / 2] : Math.round((sorted[len / 2] + sorted[len / 2 - 1]) / 2);
+        var description =
+          'digest ' + sorted[0] + 'ms ' + median + 'ms ' + sorted[len - 1] + 'ms ' +
+          (toggle ? '\u25cf' : '\u25cb');
+        summaryElement.text(description);
+      }
+
+      function instrumentedEvalAsync(expression, locals) {
+        // jshint validthis:true
+        var timing = createTiming('$evalAsync(' + formatExpression(expression) + ')');
+        originalEvalAsync.call(
+          this, wrapExpression(expression, timing, 'handle', true, true), locals);
+      }
+
+      function instrumentedApplyAsync(expression) {
+        // jshint validthis:true
+        var timing = createTiming('$applyAsync(' + formatExpression(expression) + ')');
+        originalApplyAsync.call(this, wrapExpression(expression, timing, 'handle', false, true));
+      }
+
+      function instrumentedWatch(watchExpression, listener, objectEquality) {
+        // jshint validthis:true
+        var timing = createTiming(keyOverride || formatExpression(watchExpression));
+        return originalWatch.call(
+          this, wrapExpression(watchExpression, timing, 'watch', true, false),
+          wrapListener(listener, timing), objectEquality);
+      }
+
+      function instrumentedWatchGroup(watchExpressions, listener) {
+        // jshint validthis:true
+        // $watchGroup delegates to $watch for each expression, so just make sure to set the group's
+        // aggregate key as the override first.
+        keyOverride = '[' + _.map(watchExpressions, formatExpression).join(', ') + ']';
+        var result = originalWatchGroup.call(this, watchExpressions, listener);
+        keyOverride = null;
+        return result;
+      }
+
+      return $delegate;
+    }]);
+
+    $provide.decorator('$parse', ['$delegate', function($delegate) {
+      return function(expression) {
+        var result = $delegate.apply(this, arguments);
+        result.exp = expression;
+        return result;
+      };
+    }]);
+
+    $provide.decorator('$q', ['$delegate', function($delegate) {
+      var proto = Object.getPrototypeOf($delegate.defer().promise);
+      var originalThen = proto.then;
+      var originalFinally = proto.finally;
+      proto.then = instrumentedThen;
+      proto.finally = instrumentedFinally;
+
+      function instrumentedThen(onFulfilled, onRejected, progressBack) {
+        // jshint validthis:true
+        return originalThen.call(
+          this,
+          wrapExpression(
+            onFulfilled, createTiming('$q(' + formatExpression(onFulfilled) + ')'), 'handle',
+            false, true),
+          wrapExpression(
+            onRejected, createTiming('$q(' + formatExpression(onRejected) + ')'), 'handle',
+            false, true),
+          wrapExpression(
+            progressBack, createTiming('$q(' + formatExpression(progressBack) + ')'), 'handle',
+            false, true)
+        );
+      }
+
+      function instrumentedFinally(callback, progressBack) {
+        // jshint validthis:true
+        return originalFinally.call(
+          this,
+          wrapExpression(
+            callback, createTiming('$q(' + formatExpression(callback) + ')'), 'handle',
+            false, true),
+          wrapExpression(
+            progressBack, createTiming('$q(' + formatExpression(callback) + ')'), 'handle',
+            false, true)
+        );
+      }
+
+      return $delegate;
+    }]);
+
+    var originalBind = angular.bind;
+    angular.bind = function(self, fn, args) {
+      var result = originalBind.apply(this, arguments);
+      result.exp = formatExpression(fn);
+      return result;
+    };
+  };
+
+  function percentage(value) {
+    if (value >= 1) return (value * 100).toFixed(1) + '%';
+    return ('\u2007\u2007' + (value * 100).toFixed(1) + '%').slice(-5);
+  }
+
+  function formatExpression(watchExpression) {
+    if (!watchExpression) return '';
+    if (_.isString(watchExpression)) return watchExpression;
+    if (_.isString(watchExpression.exp)) return watchExpression.exp;
+    if (watchExpression.name) return 'function ' + watchExpression.name + '() {\u2026}';
+    return watchExpression.toString();
+  }
+
+  function wrapExpression(expression, timing, counter, flushCycle, endCycle) {
+    if (!expression && !flushCycle) return expression;
+    var actualExpression = expression;
+    if (_.isString(expression)) {
+      actualExpression = function(scope) {return scope.$eval(expression);};
+    }
+    return function instrumentedExpression() {
+      if (flushCycle) flushTimingCycle();
+      if (!actualExpression) return;
+      if (!inDigest) return actualExpression.apply(this, arguments);
+      var start = Date.now();
+      timing.startCycle(start);
+      try {
+        return actualExpression.apply(this, arguments);
+      } finally {
+        timing.countTime(counter, Date.now() - start);
+        if (endCycle) timing.endCycle();
+      }
+    };
+  }
+
+  function wrapListener(listener, timing) {
+    if (!listener) return listener;
+    return function instrumentedListener() {
+      var start = Date.now();
+      try {
+        return listener.apply(this, arguments);
+      } finally {
+        timing.countTime('handle', Date.now() - start);
+      }
+    };
+  }
+
+  function createTiming(key) {
+    var timing = watchTimings[key];
+    if (!timing) timing = watchTimings[key] = new WatchTiming(key);
+    return timing;
+  }
+
+  this.$get = function() {};
+});
+
